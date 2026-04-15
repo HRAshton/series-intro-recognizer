b585bf3e53a56556654d31a47129a77c4c5776df
HRAshton <12210721+hrashton@users.noreply.github.com>
2026-04-15 13:10:58 +0200

    chore: Fix path resolving in pytest, configure it using pyproject

--- tests/integration/test_audio_files.py
+++ tests/integration/test_audio_files.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 import numpy as np
 import pytest
@@ -18,52 +18,43 @@ testdata: list[tuple[float | None, float | None, tuple[float, float]]] = [
 ]
 
 
-def test__recognise_from_audio_files() -> None:
+def test__recognise_from_audio_files(tmp_path: Path) -> None:
     cfg = Config()
 
-    files = [f'assets/out/test_recognise_from_audio_files_with_offsets{i}.wav'
-             for i in range(10)]
+    files = [str(tmp_path / f'episode{i}.wav') for i in range(10)]
 
-    try:
-        common_wave = np.random.default_rng(0).random(cfg.min_segment_length_beats * 2)
-        for i in range(len(files)):
-            wave = np.random.default_rng(i + 1).random(cfg.min_segment_length_beats * 9)
-            wave[cfg.min_segment_length_beats * 3:cfg.min_segment_length_beats * 5] = common_wave
-            sf.write(files[i], wave, cfg.rate)
+    common_wave = np.random.default_rng(0).random(cfg.min_segment_length_beats * 2)
+    for i, path in enumerate(files):
+        wave = np.random.default_rng(i + 1).random(cfg.min_segment_length_beats * 9)
+        wave[cfg.min_segment_length_beats * 3:cfg.min_segment_length_beats * 5] = common_wave
+        sf.write(path, wave, cfg.rate)
 
-        # noinspection PyTypeChecker
-        result = recognise_from_audio_files(iter(files), cfg)
+    # noinspection PyTypeChecker
+    result = recognise_from_audio_files(iter(files), cfg)
 
-        print(result)
-        assert len(result) == 10
-        for interval in result:
-            assert interval == testdata[0][2]
-    finally:
-        for file in files:
-            os.remove(file)
+    print(result)
+    assert len(result) == 10
+    for interval in result:
+        assert interval == testdata[0][2]
 
 
 @pytest.mark.parametrize('offset, duration, expected_interval', testdata)
 def test__recognise_from_audio_files_with_offsets(offset: float | None, duration: float | None,
-                                                  expected_interval: tuple[float, float]) -> None:
+                                                  expected_interval: tuple[float, float],
+                                                  tmp_path: Path) -> None:
     cfg = Config()
 
-    files = [(f'assets/out/test_recognise_from_audio_files_with_offsets{i}.wav', offset, duration)
-             for i in range(10)]
+    files = [(str(tmp_path / f'episode{i}.wav'), offset, duration) for i in range(10)]
 
-    try:
-        common_wave = np.random.default_rng(0).random(cfg.min_segment_length_beats * 2)
-        for i in range(len(files)):
-            wave = np.random.default_rng(i + 1).random(cfg.min_segment_length_beats * 9)
-            wave[cfg.min_segment_length_beats * 3:cfg.min_segment_length_beats * 5] = common_wave
-            sf.write(files[i][0], wave, cfg.rate)
+    common_wave = np.random.default_rng(0).random(cfg.min_segment_length_beats * 2)
+    for i, (path, _, _) in enumerate(files):
+        wave = np.random.default_rng(i + 1).random(cfg.min_segment_length_beats * 9)
+        wave[cfg.min_segment_length_beats * 3:cfg.min_segment_length_beats * 5] = common_wave
+        sf.write(path, wave, cfg.rate)
 
-        result = recognise_from_audio_files_with_offsets(iter(files), cfg)
+    result = recognise_from_audio_files_with_offsets(iter(files), cfg)
 
-        print(result)
-        assert len(result) == 10
-        assert all(interval.start == expected_interval[0] for interval in result)
-        assert all(interval.end == expected_interval[1] for interval in result)
-    finally:
-        for file, _, _ in files:
-            os.remove(file)
+    print(result)
+    assert len(result) == 10
+    assert all(interval.start == expected_interval[0] for interval in result)
+    assert all(interval.end == expected_interval[1] for interval in result)
